d7be25183a39ed43806168cdff50aa6c87b1cba7
Alejandro <Alejandrehl@icloud.com>
2018-11-27 23:09:28 -0300

    Socket.io added to server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
+
+
 const app = express();
 
-server = app.listen(3001, function(){
-    console.log('server is running on port 3001')
+
+
+const server = app.listen(3001, function() {
+    console.log('server running on port 3001');
+});
+
+
+const io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+    console.log(socket.id)
 });
