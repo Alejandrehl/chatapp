2181ddbc0a463db09840d8087f8a0c12f8c299cd
Alejandro <Alejandrehl@icloud.com>
2018-11-27 22:44:23 -0300

    first commit

---
+++ src/components/Chat.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card mt-3">
+      <div class="card-body">
+          <div class="card-title">
+              <h3>Chat Group</h3>
+              <hr>
+          </div>
+          <div class="card-body">
+              <div class="messages">
+              </div>
+          </div>
+      </div>
+      <div class="card-footer">
+          <form @submit.prevent="sendMessage">
+              <div class="gorm-group">
+                  <label for="user">User:</label>
+                  <input type="text" v-model="user" class="form-control">
+              </div>
+              <div class="gorm-group pb-3">
+                  <label for="message">Message:</label>
+                  <input type="text" v-model="message" class="form-control">
+              </div>
+              <button type="submit" class="btn btn-success">Send</button>
+          </form>
+      </div>
+  </div>
+</template>
